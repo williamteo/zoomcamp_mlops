deaa3229ed5ebaa8de670ce596aa25d2211ed5f5
William Teo <william-teo@outlook.com>
2023-08-13 19:36:50 +0800

    Finished 04-web-service

--- 04-deployment/web-service/test.py
+++ 04-deployment/web-service/test.py
@@ -1,11 +1,11 @@
 import requests
 
 ride = {
-    "PULocationID": 10,
-    "DOLocationID": 50,
-    "trip_distance": 40
+    "PUlocationID": 10,
+    "DOlocationID": 50,
+    "trip_distance": 40,
 }
 
-url = 'http://localhost:9696/predict'
+url = "http://localhost:9696/predict"
 response = requests.post(url, json=ride)
-print(response.json())
+print(response.json()) 
